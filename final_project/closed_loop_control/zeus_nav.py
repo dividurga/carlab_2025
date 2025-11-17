083ff92c5fed7cfc37c6bd94f4c2d227934d711d
dividurga <durgadivija@gmail.com>
2025-11-17 17:22:13 -0500

    cynthia zhang is so hot and square

--- final_project/closed_loop_control/zeus_nav.py
+++ final_project/closed_loop_control/zeus_nav.py
@@ -1,4 +1,3 @@
-# zeus_nav.py — distance-based forward power ONLY
 import numpy as np
 import socket, time
 from aruco_localization import get_pose
@@ -13,7 +12,7 @@ def move_to_points(points, s, video):
     # ---------------- TUNABLE CONSTANTS ----------------
     MAX_POWER = 100     # highest forward PWM
     MIN_POWER = 0     # minimum pwm to move
-    K_DIST = 1.75      # controls how fast power scales with distance
+    K_DIST = 25      # controls how fast power scales with distance
     STOP_DIST = 1.0    # inches tolerance
 
     idx = 0
@@ -44,14 +43,19 @@ def move_to_points(points, s, video):
             continue
 
         # ------------ FORWARD POWER ------------
-        raw_power = K_DIST * dist
+        raw_power = K_DIST * np.pow(dist, 0.3)
         power = int(np.clip(raw_power, MIN_POWER, MAX_POWER))
 
         print(f"Power = {power}   (raw {raw_power:.2f})")
+        # okay lets do angle stuff now
+
+        desired_angle = np.degrees(np.arctan2(diff[1], diff[0]))
+        angle_error = 90 - (desired_angle + theta)
+
 
         # ------------ SEND COMMAND -------------
         # MOVE <angle> <power> <rot> <drift>
         # angle = 0 (forward), rot=0 (no turning)
-        cmd = f"MOVE 0 {power} 0 0"
+        cmd = f"MOVE {int(angle_error)} {power} 0 0"
         send_cmd(s, cmd)
         print("CMD:", cmd)
